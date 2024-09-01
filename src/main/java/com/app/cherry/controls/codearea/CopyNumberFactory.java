1d2804cef9b7fc782a18b43194b09cc6b6598c68
user <vaganov.kostya2000@yandex.ru>
2024-09-01 21:19:03 +0500

    add paste button

--- src/main/java/com/app/cherry/controls/codearea/CopyNumberFactory.java
+++ src/main/java/com/app/cherry/controls/codearea/CopyNumberFactory.java
@@ -6,6 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.HBox;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.model.Paragraph;
 import org.kordamp.ikonli.javafx.FontIcon;
@@ -16,24 +17,40 @@ import java.util.function.IntFunction;
 public class CopyNumberFactory implements IntFunction<Node> {
     private CodeArea codeArea;
 
+    public void setCodeArea(CodeArea codeArea) {
+        this.codeArea = codeArea;
+    }
+
     @Override
     public Node apply(int lineNumber) {
-        Button button = new Button(null, new FontIcon("mdal-content_copy"));
-        button.getStyleClass().addAll(Styles.BUTTON_CIRCLE, Styles.ACCENT);
-        button.setAlignment(Pos.CENTER);
-        button.setScaleX(0.5);
-        button.setScaleY(0.5);
-        button.setOnMouseClicked(mouseEvent -> {
+        Button copyButton = new Button(null, new FontIcon("mdal-content_copy"));
+        setStyleButton(copyButton);
+        copyButton.setOnMouseClicked(mouseEvent -> {
             Paragraph<Collection<String>, String, Collection<String>> paragraph = codeArea.getParagraph(lineNumber);
             Clipboard clipboard = Clipboard.getSystemClipboard();
             ClipboardContent content = new ClipboardContent();
             content.putString(paragraph.getText());
             clipboard.setContent(content);
         });
-        return button;
+        copyButton.setTranslateX(-5);
+
+        Button pasteButton = new Button(null, new FontIcon("mdal-content_paste"));
+        setStyleButton(pasteButton);
+        pasteButton.setOnMouseClicked(mouseEvent -> {
+            Clipboard clipboard = Clipboard.getSystemClipboard();
+            String pasteText = clipboard.getString();
+            Paragraph<Collection<String>, String, Collection<String>> paragraph = codeArea.getParagraph(lineNumber);
+            codeArea.insertText(lineNumber, 0, pasteText);
+        });
+
+        return new HBox(pasteButton, copyButton);
     }
 
-    public void setCodeArea(CodeArea codeArea) {
-        this.codeArea = codeArea;
+    private void setStyleButton(Button button) {
+        button.getStyleClass().addAll(Styles.BUTTON_CIRCLE, Styles.ACCENT);
+        button.setAlignment(Pos.CENTER);
+        button.setScaleX(0.5);
+        button.setScaleY(0.5);
     }
+
 }
